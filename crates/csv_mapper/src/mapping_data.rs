00c19a05d3307188eb8d875ea1df6f01eb62bcf8
joris <joris.willems@impierce.com>
2024-05-23 19:59:40 +0700

    update mappingrule

--- crates/csv_mapper/src/mapping_data.rs
+++ crates/csv_mapper/src/mapping_data.rs
@@ -2,9 +2,39 @@ use core::slice::Iter;
 
 #[derive(Debug)]
 pub struct MappingRule {
-    pub src_path: String,
-    pub target_path: String,
+    pub source: Vec<MappingData>,
+    pub target: Vec<MappingData>,
     pub transformation: String,
+    pub rule_number: usize, 
+}
+
+
+//source, AchievementSubject, type, AchievementSubjectType, 1|*, false, 
+//source, AchievementSubjectType, name, Contact, 1, false
+
+
+
+////0, source, Contact, phone
+
+////0, target, Subject, credential, CredentialSubject, 1, false,
+////0, target, CredentialSUbject, person, Contact, 1, false
+////0, target, Contact, phone
+
+//1, source, AchievementSubject, type, AchievementSubjectType, 1, false
+
+
+//let mapping_rule =  column[0] 
+
+//HashMap<usize, MappingRule>;
+
+
+
+
+fn to_json_path(source: MappingRule) {
+        //$.type.name.phone = "023423432";
+
+    let src_json_path = "$.type.name.phone";
+    let target_json_path = "$.credential.person.phone";
 }
 
 #[derive(Debug)]
